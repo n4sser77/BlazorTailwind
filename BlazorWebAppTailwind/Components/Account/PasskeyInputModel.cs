eb16f108fc2ffd30fef271a91c5d5ac8e0f8bf2a
Naser Al-Asbahi <nasserqahtan0@gmail.com>
2025-12-15 01:24:53 +0100

    Inital commit with tailwind v4 setup

---
+++ BlazorWebAppTailwind/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace BlazorWebAppTailwind.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ BlazorWebAppTailwind/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace BlazorWebAppTailwind.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
